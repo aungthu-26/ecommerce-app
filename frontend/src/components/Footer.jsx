f2b68ffb2b8604e6cf93156b32adaccdfbcf6245
Aung Thu <dev104.ovmyanmar@gmail.com>
2025-09-15 17:03:12 +0630

    feat: add product list and details

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <div>
+            <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+                <div>
+                    <img className='mb-5 w-32' src={assets.logo} alt="logo" />
+                    <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis accusantium amet commodi quasi magni beatae.</p>
+                </div>
+
+                <div>
+                    <p className="text-xl font-medium mb-5">COMPANY</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
+                        <li>+959-000-111-222</li>
+                        <li>contact@foreveryou.com</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright {currentYear}@ forever.com - All Right Reserved.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
