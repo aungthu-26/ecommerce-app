d1767c3c2504ecf5a17806a65ed6b0f698df5ab3
Aung Thu <dev104.ovmyanmar@gmail.com>
2025-09-16 15:19:36 +0630

    feat: add login,aboutus,contact,cart page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
     const { productId } = useParams();
-    const { products, currency } = useContext(ShopContext);
+    const { products, currency, addToCart } = useContext(ShopContext);
     const [productData, setProductData] = useState('');
     const [image, setImage] = useState('');
+    const [size, setSize] = useState('');
 
     const fetchProductData = async () => {
         products.map((item) => {
@@ -60,8 +62,57 @@ const Product = () => {
                         <p className="pl-2">(122)</p>
                     </div>
                     <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
+                    <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+                    <div className="flex flex-col gap-4 my-8">
+                        <p>Select Size</p>
+                        <div className="flex gap-2">
+                            {
+                                productData.sizes.map((item, index) => (
+                                    <button
+                                        onClick={() => setSize(item)}
+                                        className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`}
+                                        key={index}
+                                    >
+                                        {item}
+                                    </button>
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    <button
+                        onClick={() => addToCart(productData._id, size)}
+                        className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+                    >
+                        ADD TO CART
+                    </button>
+                    <hr className='mt-8 sm:w-4/5' />
+                    <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+                        <p>100% Original product.</p>
+                        <p>Cash on delivery is availble on this product.</p>
+                        <p>Easy return and exchange policy within 7 days.</p>
+                    </div>
+
                 </div>
             </div>
+
+            {/* description and review seciton */}
+            <div className="mt-20">
+                <div className="flex">
+                    <b className="border px-5 py-3 text-sm">Description</b>
+                    <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+                </div>
+                <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet enim esse omnis quia iste saepe voluptatem totam nemo facere ad! Rem molestiae maiores fuga non ab quia est voluptas dolores.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque officiis dicta fugit praesentium aut alias, quia, sit possimus doloremque veniam hic iusto earum in, culpa harum deserunt esse nemo inventore. Provident explicabo eum facilis optio.</p>
+                </div>
+            </div>
+
+            {/* display related products */}
+            <RelatedProducts
+                category={productData.category}
+                subCategory={productData.subCategory}
+            />
 
         </div>
     ) : <div className='opacity-0'></div>
